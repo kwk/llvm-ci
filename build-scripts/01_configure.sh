202298b87641401c5c5b6ed6524822ef6b400672
Konrad Kleine <kkleine@redhat.com>
2020-09-03 20:50:35 +0200

    cleanup configure script

--- build-scripts/01_configure.sh
+++ build-scripts/01_configure.sh
@@ -75,22 +75,3 @@ CMD="$CMD -DCMAKE_EXPORT_COMPILE_COMMANDS=${CMAKE_EXPORT_COMPILE_COMMANDS}"
 
 echo "--- Configure and Generate"
 eval "$CMD"
-
-export PATH="${PATH}:${PWD}/bin"
-
-# Build all configured projects (see LLVM_ENABLE_PROJECTS above)
-echo "--- Build"
-cmake --build . --config "${CMAKE_BUILD_TYPE}" --target all
-
-# See https://llvm.org/docs/CMake.html#executing-the-tests
-echo "--- Test"
-cmake --build . --config "${CMAKE_BUILD_TYPE}" --target check-all
-
-echo "--- Clang Tidy"
-git diff -U0 --no-prefix HEAD~1 | clang-tidy-diff -p0
-
-echo "--- Clang Format"
-../clang/tools/clang-format/git-clang-format HEAD~1
-
-echo "--- Show CCache statistics" 
-ccache --show-stats
