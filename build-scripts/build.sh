610ac68bcbd733d47bcdf5d6e0c64dd4aae3cf5e
Konrad Kleine <kkleine@redhat.com>
2020-09-04 14:23:35 +0200

    Change artifacts dir

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -8,16 +8,17 @@ set -e
 # in the pipeline.
 set -o pipefail
 
-mkdir -p artifacts
+ARTIFACTS_DIR=$PWD/artifacts
+mkdir -p ${ARTIFACTS_DIR}
 
 echo "--- Clear CCache (make it cold)"
-ccache --clear 2>&1 | tee -a artifacts/bootstrap.log
+ccache --clear 2>&1 | tee -a ${ARTIFACTS_DIR}/bootstrap.log
 
 echo "--- Clean CCache Stats"
-ccache --zero-stats 2>&1 | tee -a artifacts/bootstrap.log
+ccache --zero-stats 2>&1 | tee -a ${ARTIFACTS_DIR}/bootstrap.log
 
 echo "--- List installed packages"
-yum list installed 2>&1 | tee -a artifacts/packages.log
+yum list installed 2>&1 | tee -a ${ARTIFACTS_DIR}/packages.log
 
 # See https://buildkite.com/docs/pipelines/managing-log-output for why we use
 # three dashes here and below.
@@ -44,23 +45,23 @@ done < <(env -0)
 [[ "${BUILD_SHARED_LIBS}" != "" ]] && CMD="$CMD -DBUILD_SHARED_LIBS=${BUILD_SHARED_LIBS}"
 
 echo "--- Configure and Generate"
-eval "$CMD" 2>&1 | tee -a artifacts/configure.log
+eval "$CMD" 2>&1 | tee -a ${ARTIFACTS_DIR}/configure.log
 
 export PATH="${PATH}:${PWD}/bin"
 
 # Build all configured projects (see LLVM_ENABLE_PROJECTS above)
 echo "--- Build"
-cmake --build . --config "${CMAKE_BUILD_TYPE}" --target all 2>&1 | tee -a artifacts/build_all.log
+cmake --build . --config "${CMAKE_BUILD_TYPE}" --target all 2>&1 | tee -a ${ARTIFACTS_DIR}/build_all.log
 
 # See https://llvm.org/docs/CMake.html#executing-the-tests
 echo "--- Test"
-cmake --build . --config "${CMAKE_BUILD_TYPE}" --target check-all 2>&1 | tee -a artifacts/check_all.log
+cmake --build . --config "${CMAKE_BUILD_TYPE}" --target check-all 2>&1 | tee -a ${ARTIFACTS_DIR}/check_all.log
 
 echo "--- Clang Tidy"
-git diff -U0 --no-prefix HEAD~1 | clang-tidy-diff -p0 2>&1 | tee -a artifacts/clang_tidy.log
+git diff -U0 --no-prefix HEAD~1 | clang-tidy-diff -p0 2>&1 | tee -a ${ARTIFACTS_DIR}/clang_tidy.log
 
 echo "--- Clang Format"
-../clang/tools/clang-format/git-clang-format HEAD~1 2>&1 | tee -a artifacts/clang_format.log
+../clang/tools/clang-format/git-clang-format HEAD~1 2>&1 | tee -a ${ARTIFACTS_DIR}/clang_format.log
 
 echo "--- Show CCache statistics" 
 ccache --show-stats 2>&1 | tee -a artifacts/ccache_stats.log
