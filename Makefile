9f3d64515b2db27fdaa71c38eea3c0771677f655
Konrad Kleine <kkleine@redhat.com>
2020-09-09 22:39:46 +0200

    Tons of work

--- Makefile
+++ Makefile
@@ -1,72 +1,66 @@
-# When you run make VERBOSE=1, executed commands will be printed before
-# executed, verbose flags are turned on and quiet flags are turned off for
-# various commands. Use V_FLAG in places where you can toggle on/off verbosity
-# using -v. Use Q_FLAG in places where you can toggle on/off quiet mode using
-# -q.
-Q = @
-Q_FLAG = -q
-V_FLAG =
-ifeq ($(VERBOSE),1)
-       Q =
-       Q_FLAG = 
-       V_FLAG = -v
-endif
-
 # It's necessary to set this because some environments don't link sh -> bash.
 SHELL := /bin/bash
 
-GIT_COMMIT_ID := $(shell git rev-parse --short HEAD)
+CI_GIT_COMMIT_ID := $(shell git rev-parse --short HEAD)
 ifneq ($(shell git status --porcelain --untracked-files=no),)
-       GIT_COMMIT_ID := $(GIT_COMMIT_ID)-dirty
+       CI_GIT_COMMIT_ID := $(CI_GIT_COMMIT_ID)-dirty
 endif
 
 ARCH=$(shell arch)
-FEDORA_32_IMAGE_NAME := quay.io/kkleine/llvm-ci:fedora-32-$(ARCH)-$(GIT_COMMIT_ID)
-RHEL_8_IMAGE_NAME := quay.io/kkleine/llvm-ci:rhel-8-$(ARCH)-$(GIT_COMMIT_ID)
-CENTOS_8_IMAGE_NAME := quay.io/kkleine/llvm-ci:centos-8-$(ARCH)-$(GIT_COMMIT_ID)
+FEDORA_32_IMAGE_NAME := quay.io/kkleine/llvm-ci:fedora-32-$(ARCH)-$(CI_GIT_COMMIT_ID)
+RHEL_8_IMAGE_NAME := quay.io/kkleine/llvm-ci:rhel-8-$(ARCH)-$(CI_GIT_COMMIT_ID)
+CENTOS_8_IMAGE_NAME := quay.io/kkleine/llvm-ci:centos-8-$(ARCH)-$(CI_GIT_COMMIT_ID)
 
 .PHONY: fedora-32-image
 fedora-32-image: Dockerfile.fedora32
 	@echo Building image ${FEDORA_32_IMAGE_NAME}
-	$(Q)podman build $(Q_FLAG) \
-		--build-arg git_revision=$(GIT_COMMIT_ID) \
-		--build-arg arch=$(ARCH) \
-		--build-arg container_image_ref=${FEDORA_32_IMAGE_NAME} \
+	podman build \
+		--build-arg ci_git_revision=$(CI_GIT_COMMIT_ID) \
+		--build-arg ci_container_image_ref=${FEDORA_32_IMAGE_NAME} \
 		. \
 		-f Dockerfile.fedora32 \
 		-t ${FEDORA_32_IMAGE_NAME}
+
+.PHONY: push-fedora-32-image
+push-fedora-32-image:
 	@echo Pushing image ${FEDORA_32_IMAGE_NAME}
-	$(Q)podman push $(Q_FLAG) ${FEDORA_32_IMAGE_NAME}
+	podman push ${FEDORA_32_IMAGE_NAME}
 
 .PHONY: centos-8-image
 centos-8-image: Dockerfile.centos8
 	@echo Building image ${CENTOS_8_IMAGE_NAME}
-	$(Q)podman build $(Q_FLAG) \
-		--build-arg git_revision=$(GIT_COMMIT_ID) \
+	podman build \
+		--build-arg ci_git_revision=$(CI_GIT_COMMIT_ID) \
+		--build-arg ci_container_image_ref=${CENTOS_8_IMAGE_NAME} \
 		--build-arg arch=$(ARCH) \
-		--build-arg container_image_ref=${CENTOS_8_IMAGE_NAME} \
 		. \
 		-f Dockerfile.centos8 \
 		-t ${CENTOS_8_IMAGE_NAME}
+
+.PHONY: push-centos-8-image
+push-centos-8-image:
 	@echo Pushing image ${CENTOS_8_IMAGE_NAME}
-	$(Q)podman push $(Q_FLAG) ${CENTOS_8_IMAGE_NAME}
+	podman push ${CENTOS_8_IMAGE_NAME}
 
 .PHONY: rhel-8-image
 rhel-8-image: Dockerfile.rhel8
 	@echo Building image ${RHEL_8_IMAGE_NAME}
-	$(Q)podman build $(Q_FLAG) \
-		--build-arg git_revision=$(GIT_COMMIT_ID) \
+	podman build \
+		--build-arg ci_git_revision=$(CI_GIT_COMMIT_ID) \
+		--build-arg ci_container_image_ref=${RHEL_8_IMAGE_NAME} \
 		--build-arg arch=$(ARCH) \
-		--build-arg container_image_ref=${RHEL_8_IMAGE_NAME} \
 		. \
 		-f Dockerfile.rhel8 \
 		-t ${RHEL_8_IMAGE_NAME}
+
+.PHONY: push-rhel-8-image
+push-rhel-8-image:
 	@echo Pushing image ${RHEL_8_IMAGE_NAME}
-	$(Q)podman push $(Q_FLAG) ${RHEL_8_IMAGE_NAME}
+	podman push ${RHEL_8_IMAGE_NAME}
 
 .PHONY: deploy
 deploy:
-	$(Q)echo -n "Logged in as "
-	$(Q)oc whoami -c
-	$(Q)oc apply --dry-run=true --overwrite=true -o  -f example-pod-config.yaml
+	echo -n "Logged in as "
+	oc whoami -c
+	oc apply --dry-run=true --overwrite=true -o  -f yaml/pod-config.yaml.sample
 
--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash 
 
-set -x
 set -e
 
 # Ensure Bash pipelines (e.g. cmd | othercmd) return a non-zero status if any of
@@ -8,20 +7,41 @@ set -e
 # in the pipeline.
 set -o pipefail
 
+GIT_REV=$(git rev-parse HEAD)
+
 ARTIFACTS_DIR=$PWD/artifacts
 mkdir -p ${ARTIFACTS_DIR}
 
-echo "" > ${ARTIFACTS_DIR}/reproduce.sh
+# We write the commands to be executed into a script to be executed later. This
+# way we actually execute the commands that we instruct a user to run when she
+# wants to reproduce this build locally.
+REPRODUCER_SCRIPT=${ARTIFACTS_DIR}/reproduce.sh
+rm -f ${REPRODUCER_SCRIPT}
+touch ${REPRODUCER_SCRIPT}
+chmod +x ${REPRODUCER_SCRIPT}
+
+ENV_SETTINGS=$(declare -p) 
+
+cat >> ${REPRODUCER_SCRIPT} <<EOL
+#!/bin/bash
+
+echo "--- Double check the correct revision is checked out"
+git checkout ${GIT_REV}
+
+# Output all environment variables to reproducer script
+echo "--- Set environment variables"
+${ENV_SETTINGS}
+
+echo "--- Prepare build and artifacts directories"
+mkdir -pv ${PWD}
+mkdir -p ${ARTIFACTS_DIR}
+cd ${PWD}
 
 echo "--- Clear CCache (make it cold)"
-CMD="ccache --clear"
-eval "$CMD" 2>&1 | tee -a ${ARTIFACTS_DIR}/bootstrap.log
-echo "$CMD" >> ${ARTIFACTS_DIR}/reproduce.sh
+ccache --clear | tee -a ${ARTIFACTS_DIR}/bootstrap.log
 
 echo "--- Clean CCache Stats"
-CMD="ccache --zero-stats"
-eval "$CMD" 2>&1 | tee -a ${ARTIFACTS_DIR}/bootstrap.log
-echo "$CMD" >> ${ARTIFACTS_DIR}/reproduce.sh
+ccache --zero-stats 2>&1 | tee -a ${ARTIFACTS_DIR}/bootstrap.log
 
 echo "--- List installed packages"
 yum list installed 2>&1 | tee -a ${ARTIFACTS_DIR}/packages.log
@@ -29,73 +49,51 @@ yum list installed 2>&1 | tee -a ${ARTIFACTS_DIR}/packages.log
 # See https://buildkite.com/docs/pipelines/managing-log-output for why we use
 # three dashes here and below.
 echo "--- Prepare CMake configuration"
-
 mkdir -pv build
-
 cd build
-
-CMD="cmake ../llvm"
-
-# CMake variables
-
-GENERATOR=${GENERATOR:-Ninja}
-CMD="$CMD -G \"$GENERATOR\""
-
-# Automatically add variables to CMAKE defines when they begin with reasonable prefixes
-while IFS='=' read -r -d '' n v; do
-    if [[ "$n" =~ ^(LLVM|CLANG|LLDB|CMAKE)_.* ]]; then
-        CMD="$CMD -D$n=\"$v\""
-    fi
-done < <(env -0)
-
-[[ "${BUILD_SHARED_LIBS}" != "" ]] && CMD="$CMD -DBUILD_SHARED_LIBS=${BUILD_SHARED_LIBS}"
 
 echo "--- Configure and Generate"
 
-echo "$CMD" >> ${ARTIFACTS_DIR}/reproduce.sh
-eval "$CMD" 2>&1 | tee -a ${ARTIFACTS_DIR}/configure.log
+MY_CMAKE_DEFINES=\${MY_CMAKE_DEFINES:-""}
+cmake ../llvm ${MY_CMAKE_DEFINES} 2>&1 | tee -a ${ARTIFACTS_DIR}/configure.log"
 
-export PATH="${PATH}:${PWD}/bin"
+export PATH=\"\${PATH}:${PWD}/bin\
 
-# Build all configured projects (see LLVM_ENABLE_PROJECTS above)
 echo "--- Build"
-CMD="cmake --build . --config "${CMAKE_BUILD_TYPE}" --target all"
-eval "$CMD" 2>&1 | tee -a ${ARTIFACTS_DIR}/build_all.log
-echo "$CMD" >> ${ARTIFACTS_DIR}/reproduce.sh
+cmake --build . --config \"${CMAKE_BUILD_TYPE}\" --target all 2>&1 | tee -a ${ARTIFACTS_DIR}/build_all.log
 
 # See https://llvm.org/docs/CMake.html#executing-the-tests
 echo "--- Test"
-CMD="cmake --build . --config "${CMAKE_BUILD_TYPE}" --target check-all"
-eval "$CMD" 2>&1 | tee -a ${ARTIFACTS_DIR}/check_all.log
-echo "$CMD" >> ${ARTIFACTS_DIR}/reproduce.sh
+cmake --build . --config \"${CMAKE_BUILD_TYPE}\" --target check-all 2>&1 | tee -a ${ARTIFACTS_DIR}/check_all.log
 
 echo "--- Clang Tidy"
-CMD="git diff -U0 --no-prefix HEAD~1 | clang-tidy-diff -p0"
-eval "$CMD" 2>&1 | tee -a ${ARTIFACTS_DIR}/clang_tidy.log
-echo "$CMD" >> ${ARTIFACTS_DIR}/reproduce.sh
+git diff -U0 --no-prefix HEAD~1 | clang-tidy-diff -p0 >> ${ARTIFACTS_DIR}/clang_tidy.log
 
 echo "--- Clang Format"
-CMD="../clang/tools/clang-format/git-clang-format HEAD~1"
-eval "$CMD" 2>&1 | tee -a ${ARTIFACTS_DIR}/clang_format.log
-echo "$CMD" >> ${ARTIFACTS_DIR}/reproduce.sh
+../clang/tools/clang-format/git-clang-format HEAD~1 2>&1 | tee -a ${ARTIFACTS_DIR}/clang_format.log
 
 echo "--- Show CCache statistics"
-CMD="ccache --show-stats"
-eval "$CMD" 2>&1 | tee -a artifacts/ccache_stats.log
-echo "$CMD" >> ${ARTIFACTS_DIR}/reproduce.sh
+ccache --show-stats 2>&1 | tee -a artifacts/ccache_stats.log
+EOL
 
-if [ "${BUILDKITE_REPO}" != "" ]; then
-    set +x
-    echo "--- Reproduce build locally"
-    echo "# Start a container:"
-    echo "podman run -it --rm --entrypoint bash ${CONTAINER_IMAGE_REF}"
-    echo "# Inside the container run:"
-    echo "su buildkite-agent"
-    echo "cd /var/lib/buildkite-agent/builds"
-    echo "mkdir -p mybuild"
-    echo "cd mybuild"
-    echo "git clone ${BUILDKITE_REPO} llvm-project"
-    echo "cd llvm-project"
-    echo "git checkout ${BUILDKITE_COMMIT}"
-    cat ${ARTIFACTS_DIR}/reproduce.sh
-fi
+source ${REPRODUCER_SCRIPT}
+
+# if [ "${BUILDKITE_REPO}" != "" ]; then
+#     set +x
+#     cat <<EOT >> ${ARTIFACTS_DIR}/reproduce-final.sh
+# #!/bin/bash
+# podman run -it --rm --entrypoint bash ${CI_CONTAINER_IMAGE_REF}
+# EOT
+#     echo "--- Reproduce build locally" >> ${ARTIFACTS_DIR}/reproduce.sh
+#     echo "# Start a container:" >> ${ARTIFACTS_DIR}/reproduce.sh
+#     echo "podman run -it --rm --entrypoint bash ${CI_CONTAINER_IMAGE_REF}" >> ${ARTIFACTS_DIR}/reproduce.sh
+#     echo "# Inside the container run:" >> ${ARTIFACTS_DIR}/reproduce.sh
+#     echo "su buildkite-agent" 
+#     echo "cd /var/lib/buildkite-agent/builds"
+#     echo "mkdir -p mybuild"
+#     echo "cd mybuild"
+#     echo "git clone ${BUILDKITE_REPO} llvm-project"
+#     echo "cd llvm-project"
+#     echo "git checkout ${BUILDKITE_COMMIT}"
+#     cat ${ARTIFACTS_DIR}/reproduce.sh
+# fi
