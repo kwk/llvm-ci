824672bf9e68c6a98c2c1b4ced091d3ccee5fc7e
Konrad Kleine <kkleine@redhat.com>
2020-09-10 13:39:00 +0200

    make bash verbose

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 
 set -e
+set -x
 
 # Ensure Bash pipelines (e.g. cmd | othercmd) return a non-zero status if any of
 # the commands fail, rather than returning the exit status of the last command
