9b56fc49b28e7099484523e5157f975bb0a0b2ce
Konrad Kleine <kkleine@redhat.com>
2020-09-11 19:37:01 +0200

    Added reproduce steps, fixed error with logs

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -35,18 +35,17 @@ echo "--- Set environment variables"
 ${ENV_SETTINGS}
 
 echo "--- Prepare build and artifacts directories"
-mkdir -pv ${PWD}
-mkdir -p ${ARTIFACTS_DIR}
+mkdir -pv "${ARTIFACTS_DIR}"
 cd ${PWD}
 
 echo "--- Clear CCache (make it cold)"
-ccache --clear | tee -a ${ARTIFACTS_DIR}/bootstrap.log
+ccache --clear | tee -a "${ARTIFACTS_DIR}/bootstrap.log"
 
 echo "--- Clean CCache Stats"
-ccache --zero-stats 2>&1 | tee -a ${ARTIFACTS_DIR}/bootstrap.log
+ccache --zero-stats 2>&1 | tee -a "${ARTIFACTS_DIR}/bootstrap.log"
 
 echo "--- List installed packages"
-yum list installed 2>&1 | tee -a ${ARTIFACTS_DIR}/packages.log
+yum list installed 2>&1 | tee -a "${ARTIFACTS_DIR}/packages.log"
 
 # See https://buildkite.com/docs/pipelines/managing-log-output for why we use
 # three dashes here and below.
@@ -57,45 +56,47 @@ cd build
 echo "--- Configure and Generate"
 
 MY_CMAKE_DEFINES=\${MY_CMAKE_DEFINES:-""}
-cmake ../llvm ${MY_CMAKE_DEFINES} 2>&1 | tee -a ${ARTIFACTS_DIR}/configure.log"
+cmake ../llvm ${MY_CMAKE_DEFINES} 2>&1 | tee -a "${ARTIFACTS_DIR}/configure.log"
 
 export PATH=\"\${PATH}:${PWD}/bin\
 
 echo "--- Build"
-cmake --build . --config \"${CMAKE_BUILD_TYPE}\" --target all 2>&1 | tee -a ${ARTIFACTS_DIR}/build_all.log
+cmake --build . --config \"${CMAKE_BUILD_TYPE}\" --target all 2>&1 | tee -a "${ARTIFACTS_DIR}/build_all.log"
 
 # See https://llvm.org/docs/CMake.html#executing-the-tests
 echo "--- Test"
-cmake --build . --config \"${CMAKE_BUILD_TYPE}\" --target check-all 2>&1 | tee -a ${ARTIFACTS_DIR}/check_all.log
+cmake --build . --config \"${CMAKE_BUILD_TYPE}\" --target check-all 2>&1 | tee -a "${ARTIFACTS_DIR}/check_all.log"
 
 echo "--- Clang Tidy"
-git diff -U0 --no-prefix HEAD~1 | clang-tidy-diff -p0 >> ${ARTIFACTS_DIR}/clang_tidy.log
+git diff -U0 --no-prefix HEAD~1 | clang-tidy-diff -p0 >> "${ARTIFACTS_DIR}/clang_tidy.log"
 
 echo "--- Clang Format"
-../clang/tools/clang-format/git-clang-format HEAD~1 2>&1 | tee -a ${ARTIFACTS_DIR}/clang_format.log
+../clang/tools/clang-format/git-clang-format HEAD~1 2>&1 | tee -a "${ARTIFACTS_DIR}/clang_format.log"
 
 echo "--- Show CCache statistics"
-ccache --show-stats 2>&1 | tee -a artifacts/ccache_stats.log
+ccache --show-stats 2>&1 | tee -a "${ARTIFACTS_DIR}/ccache_stats.log"
 EOL
 
 source ${REPRODUCER_SCRIPT}
 
-# if [ "${BUILDKITE_REPO}" != "" ]; then
-#     set +x
-#     cat <<EOT >> ${ARTIFACTS_DIR}/reproduce-final.sh
-# #!/bin/bash
-# podman run -it --rm --entrypoint bash ${CI_CONTAINER_IMAGE_REF}
-# EOT
-#     echo "--- Reproduce build locally" >> ${ARTIFACTS_DIR}/reproduce.sh
-#     echo "# Start a container:" >> ${ARTIFACTS_DIR}/reproduce.sh
-#     echo "podman run -it --rm --entrypoint bash ${CI_CONTAINER_IMAGE_REF}" >> ${ARTIFACTS_DIR}/reproduce.sh
-#     echo "# Inside the container run:" >> ${ARTIFACTS_DIR}/reproduce.sh
-#     echo "su buildkite-agent" 
-#     echo "cd /var/lib/buildkite-agent/builds"
-#     echo "mkdir -p mybuild"
-#     echo "cd mybuild"
-#     echo "git clone ${BUILDKITE_REPO} llvm-project"
-#     echo "cd llvm-project"
-#     echo "git checkout ${BUILDKITE_COMMIT}"
-#     cat ${ARTIFACTS_DIR}/reproduce.sh
-# fi
+# TODO(kwk): also output steps to reproduce when running on buildbot
+if [ "${BUILDKITE_AGENT_ACCESS_TOKEN}" != "" ]; then
+    cat <<EOT
+#!/bin/bash
+podman run -it --rm --entrypoint bash ${CI_CONTAINER_IMAGE_REF}
+EOT
+cat <<EOT
+--- Reproduce build locally"
+# Download $(basename ${ARTIFACTS_DIR})/reproduce.sh
+# TODO(kwk): Maybe utilize BUILDKITE_BUILD_URL to generate wget-table URL to reproduce.sh script
+
+# Run container and mount the LLVM codebase as well as the the reproduce script.
+podman run -it --rm \
+    -v <PATH_TO_LLVM_TREE>:${BUILDKITE_BUILD_CHECKOUT_PATH}:Z \
+    -w ${BUILDKITE_BUILD_CHECKOUT_PATH} \
+    -v ${PWD}/build-scripts/buildbot.sh:/reproduce.sh:Z \
+    -u \$(shell id -u \$(USER)):\$(shell id -g \$(USER)) \
+    ${CI_CONTAINER_IMAGE_REF} \
+    /reproduce.sh
+EOT
+fi
