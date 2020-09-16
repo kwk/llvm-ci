66d1932a069d5aae2e0d000be5441f46b656a412
Konrad Kleine <kkleine@redhat.com>
2020-09-16 23:10:22 +0200

    Don't fail the build.sh but write to artifacts/error

--- bin/build.sh
+++ bin/build.sh
@@ -1,12 +1,6 @@
 #!/bin/bash 
 
-set -e
 set -x
-
-# Ensure Bash pipelines (e.g. cmd | othercmd) return a non-zero status if any of
-# the commands fail, rather than returning the exit status of the last command
-# in the pipeline.
-set -o pipefail
 
 GIT_REV=$(git rev-parse HEAD)
 
@@ -29,9 +23,12 @@ ENV_SETTINGS=$(declare -p \
 cat >> ${REPRODUCER_SCRIPT} <<EOL
 #!/bin/bash
 
+save_error() {
+    echo "$@" | tee ${ARTIFACTS_DIR}/error
+}
+
 # Output all environment variables to reproducer script
 echo "--- Set environment variables"
-# TODO(kwk): Based on Serge's suggestion I should start with a fresh env (see env -i bash)
 ${ENV_SETTINGS}
 
 echo "--- Prepare build and artifacts directories"
@@ -40,12 +37,15 @@ cd ${PWD}
 
 echo "--- Clear CCache (make it cold)"
 ccache --clear | tee -a "${ARTIFACTS_DIR}/bootstrap.log"
+[[ $? != 0 ]] save_error "clear ccache failed" && exit 0
 
 echo "--- Clean CCache Stats"
 ccache --zero-stats 2>&1 | tee -a "${ARTIFACTS_DIR}/bootstrap.log"
+[[ $? != 0 ]] save_error "zero stats in ccache failed" && exit 0
 
 echo "--- List installed packages"
 yum list installed 2>&1 | tee -a "${ARTIFACTS_DIR}/packages.log"
+[[ $? != 0 ]] save_error "list installed packages failed" && exit 0
 
 # See https://buildkite.com/docs/pipelines/managing-log-output for why we use
 # three dashes here and below.
@@ -57,24 +57,30 @@ echo "--- Configure and Generate"
 
 MY_CMAKE_DEFINES=\${MY_CMAKE_DEFINES:-""}
 cmake ../llvm ${MY_CMAKE_DEFINES} 2>&1 | tee -a "${ARTIFACTS_DIR}/configure.log"
+[[ $? != 0 ]] save_error "cmake configuration failed" && exit 0
 
 export PATH=\"\${PATH}:${PWD}/bin\
 
 echo "--- Build"
 cmake --build . --config \"${CMAKE_BUILD_TYPE}\" --target all 2>&1 | tee -a "${ARTIFACTS_DIR}/build_all.log"
+[[ $? != 0 ]] save_error "build all failed" && exit 0
 
 # See https://llvm.org/docs/CMake.html#executing-the-tests
 echo "--- Test"
 cmake --build . --config \"${CMAKE_BUILD_TYPE}\" --target check-all 2>&1 | tee -a "${ARTIFACTS_DIR}/check_all.log"
+[[ $? != 0 ]] save_error "check all failed" && exit 0
 
 echo "--- Clang Tidy"
 git diff -U0 --no-prefix HEAD~1 | clang-tidy-diff -p0 >> "${ARTIFACTS_DIR}/clang_tidy.log"
+[[ $? != 0 ]] save_error "clang tidy failed" && exit 0
 
 echo "--- Clang Format"
 ../clang/tools/clang-format/git-clang-format HEAD~1 2>&1 | tee -a "${ARTIFACTS_DIR}/clang_format.log"
+[[ $? != 0 ]] save_error "git clang format failed" && exit 0
 
 echo "--- Show CCache statistics"
 ccache --show-stats 2>&1 | tee -a "${ARTIFACTS_DIR}/ccache_stats.log"
+[[ $? != 0 ]] save_error "ccache show stats failed" && exit 0
 EOL
 
 source ${REPRODUCER_SCRIPT}
--- bin/uid_entrypoint.sh
+++ bin/uid_entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# See: https://docs.openshift.com/container-platform/3.3/creating_images/guidelines.html#openshift-container-platform-specific-guidelines
+
 if ! whoami &> /dev/null; then
   if [ -w /etc/passwd ]; then
     echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/sbin/nologin" >> /etc/passwd
