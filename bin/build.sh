093b7bf1f3fdefaf38643fe8e8e923edb0ded9ec
Konrad Kleine <kkleine@redhat.com>
2020-09-21 18:23:06 +0200

    Fix bash error: mising &&

--- bin/build.sh
+++ bin/build.sh
@@ -37,15 +37,15 @@ cd ${PWD}
 
 echo "--- Clear CCache (make it cold)"
 ccache --clear | tee -a "${ARTIFACTS_DIR}/bootstrap.log"
-[[ $? != 0 ]] save_error "clear ccache failed" && exit 0
+[[ $? != 0 ]] && save_error "clear ccache failed" && exit 0
 
 echo "--- Clean CCache Stats"
 ccache --zero-stats 2>&1 | tee -a "${ARTIFACTS_DIR}/bootstrap.log"
-[[ $? != 0 ]] save_error "zero stats in ccache failed" && exit 0
+[[ $? != 0 ]] && save_error "zero stats in ccache failed" && exit 0
 
 echo "--- List installed packages"
 yum list installed 2>&1 | tee -a "${ARTIFACTS_DIR}/packages.log"
-[[ $? != 0 ]] save_error "list installed packages failed" && exit 0
+[[ $? != 0 ]] && save_error "list installed packages failed" && exit 0
 
 # See https://buildkite.com/docs/pipelines/managing-log-output for why we use
 # three dashes here and below.
@@ -57,30 +57,30 @@ echo "--- Configure and Generate"
 
 MY_CMAKE_DEFINES=\${MY_CMAKE_DEFINES:-""}
 cmake ../llvm ${MY_CMAKE_DEFINES} 2>&1 | tee -a "${ARTIFACTS_DIR}/configure.log"
-[[ $? != 0 ]] save_error "cmake configuration failed" && exit 0
+[[ $? != 0 ]] && save_error "cmake configuration failed" && exit 0
 
 export PATH=\"\${PATH}:${PWD}/bin\
 
 echo "--- Build"
 cmake --build . --config \"${CMAKE_BUILD_TYPE}\" --target all 2>&1 | tee -a "${ARTIFACTS_DIR}/build_all.log"
-[[ $? != 0 ]] save_error "build all failed" && exit 0
+[[ $? != 0 ]] && save_error "build all failed" && exit 0
 
 # See https://llvm.org/docs/CMake.html#executing-the-tests
 echo "--- Test"
 cmake --build . --config \"${CMAKE_BUILD_TYPE}\" --target check-all 2>&1 | tee -a "${ARTIFACTS_DIR}/check_all.log"
-[[ $? != 0 ]] save_error "check all failed" && exit 0
+[[ $? != 0 ]] && save_error "check all failed" && exit 0
 
 echo "--- Clang Tidy"
 git diff -U0 --no-prefix HEAD~1 | clang-tidy-diff -p0 >> "${ARTIFACTS_DIR}/clang_tidy.log"
-[[ $? != 0 ]] save_error "clang tidy failed" && exit 0
+[[ $? != 0 ]] && save_error "clang tidy failed" && exit 0
 
 echo "--- Clang Format"
 ../clang/tools/clang-format/git-clang-format HEAD~1 2>&1 | tee -a "${ARTIFACTS_DIR}/clang_format.log"
-[[ $? != 0 ]] save_error "git clang format failed" && exit 0
+[[ $? != 0 ]] && save_error "git clang format failed" && exit 0
 
 echo "--- Show CCache statistics"
 ccache --show-stats 2>&1 | tee -a "${ARTIFACTS_DIR}/ccache_stats.log"
-[[ $? != 0 ]] save_error "ccache show stats failed" && exit 0
+[[ $? != 0 ]] && save_error "ccache show stats failed" && exit 0
 EOL
 
 source ${REPRODUCER_SCRIPT}
