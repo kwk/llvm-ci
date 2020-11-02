3a6fcc05a0f4f41d72cbd4e774bab9c138f9c63d
Konrad Kleine <kkleine@redhat.com>
2020-11-02 22:42:47 +0100

    Simplify worker deployment

--- worker/worker.mk
+++ worker/worker.mk
@@ -24,18 +24,15 @@ push-worker-image:
 run-local-worker: worker-image
 	$(CONTAINER_TOOL) run -it --rm ${WORKER_IMAGE} bash 
 
-
-.PHONY: deploy-worker-secrets
-## Deletes and recreates the secrets of the workers for buildkite and buildbot on a Kubernetes cluster.
-deploy-worker-secrets:
-	-kubectl delete secret --force=true --grace-period=0 buildkite-secret
-	-kubectl delete secret --force=true --grace-period=0 buildbot-worker-secret
-	kubectl apply --dry-run=false --overwrite=true -f ./worker/k8s/buildbot-secret.yaml
-	kubectl apply --dry-run=false --overwrite=true -f ./worker/k8s/buildbot-secret.yaml
+.PHONY: delete-worker-deployment
+## Removes all parts of the buildbot worker deployment from the cluster
+delete-worker-deployment:
+	-kubectl delete pod,secret -l app=worker
 
 .PHONY: deploy-worker
 ## Deletes and recreates the worker container image as a pod on a Kubernetes cluster.
-deploy-worker: deploy-worker-secrets
-	-kubectl delete pod --force=true --grace-period=0 worker-pod
-	sed 's|PLACE_IMAGE_HERE|${WORKER_IMAGE}|g' ./worker/k8s/pod.yaml > ./out/worker-pod.yaml
-	kubectl apply --dry-run=false --overwrite=true -f ./out/worker-pod.yaml
+deploy-worker: delete-worker-deployment
+	kubectl apply -f ./worker/k8s/buildbot-secret.yaml
+	kubectl apply -f ./worker/k8s/buildkite-secret.yaml
+	envsubst '$${WORKER_IMAGE}' < ./worker/k8s/pod.yaml > ./out/worker-pod.yaml
+	kubectl apply -f ./out/worker-pod.yaml
