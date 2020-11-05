cc328b548b81b73ecd6645e1fd4436a0945081c0
Konrad Kleine <kkleine@redhat.com>
2020-11-05 15:13:24 +0100

    Always fresh deployment, no overwrite needed

--- worker/worker.mk
+++ worker/worker.mk
@@ -45,10 +45,10 @@ delete-worker-deployment:
 .PHONY: deploy-worker
 ## Deletes and recreates the worker container image as a pod on a Kubernetes cluster.
 deploy-worker: ready-to-deploy worker-image push-worker-image delete-worker-deployment
-	kubectl apply -f --overwrite=true ./worker/k8s/buildbot-secret.yaml
-	kubectl apply -f --overwrite=true ./worker/k8s/buildkite-secret.yaml
+	kubectl apply -f ./worker/k8s/buildbot-secret.yaml
+	kubectl apply -f ./worker/k8s/buildkite-secret.yaml
 	export WORKER_IMAGE=$(WORKER_IMAGE) \
 	&& export BUILDBOT_MASTER="$(shell kubectl get route master-route-www -o json | jq -j '"http://"+.spec.host+.spec.path')" \
 	&& export BUILDBOT_ACCESS_URI="https://console-openshift-console.apps.ocp.prod.psi.redhat.com/k8s/ns/llvm-pre-merge/pods/worker-pod" \
 	&& envsubst '$${WORKER_IMAGE} $${BUILDBOT_MASTER} $${BUILDBOT_ACCESS_URI}' < ./worker/k8s/pod.yaml > ./out/worker-pod.yaml
-	kubectl apply -f --overwrite=true ./out/worker-pod.yaml
+	kubectl apply -f ./out/worker-pod.yaml
