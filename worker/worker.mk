8b6d7c73c6a3c78c3b5139a9c0a553d015126263
Konrad Kleine <kkleine@redhat.com>
2020-11-05 15:16:36 +0100

    Pick correct route

--- worker/worker.mk
+++ worker/worker.mk
@@ -48,7 +48,7 @@ deploy-worker: ready-to-deploy worker-image push-worker-image delete-worker-depl
 	kubectl apply -f ./worker/k8s/buildbot-secret.yaml
 	kubectl apply -f ./worker/k8s/buildkite-secret.yaml
 	export WORKER_IMAGE=$(WORKER_IMAGE) \
-	&& export BUILDBOT_MASTER="$(shell kubectl get route master-route-www -o json | jq -j '"http://"+.spec.host+.spec.path')" \
+	&& export BUILDBOT_MASTER="$(shell kubectl get route master-route-workers -o json | jq -j '"http://"+.spec.host+.spec.path')" \
 	&& export BUILDBOT_ACCESS_URI="https://console-openshift-console.apps.ocp.prod.psi.redhat.com/k8s/ns/llvm-pre-merge/pods/worker-pod" \
 	&& envsubst '$${WORKER_IMAGE} $${BUILDBOT_MASTER} $${BUILDBOT_ACCESS_URI}' < ./worker/k8s/pod.yaml > ./out/worker-pod.yaml
 	kubectl apply -f ./out/worker-pod.yaml
