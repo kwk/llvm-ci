4bdd4ba9c7b1687880f587413e5628af50ca04c4
Konrad Kleine <kkleine@redhat.com>
2021-01-20 14:40:14 +0100

    fixup

--- .github/workflows/js/add_issue_comment.js
+++ .github/workflows/js/add_issue_comment.js
@@ -1,6 +1,6 @@
 // Takes an issue's/PR's node ID and a comment body and creates a new comment.
 // It returns the ID node of the newly created comment.
-module.exports = (github, issue_node_id, comment_body) => {
+module.exports = async (github, issue_node_id, comment_body) => {
     const query = `mutation($issue_node_id:String!, $comment_body:String!) {
         addComment(input: {subjectId:$issue_node_id, body:$comment_body}) {
             commentEdge{
