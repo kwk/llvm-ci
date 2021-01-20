108d078f45f4d211e8a6d33fef290315592bb3b4
Konrad Kleine <kkleine@redhat.com>
2021-01-20 23:17:04 +0100

    fixup

--- .github/workflows/js/update_issue_comment.js
+++ .github/workflows/js/update_issue_comment.js
@@ -1,16 +1,14 @@
 // update_issue_comment:
 //
 // Updates an issue/PR comment with a new comment body. It returns the
-// ID node of updated comment.
+// last edited field.
 module.exports = async (github, comment_node_id, comment_body) => {
     
     // see https://docs.github.com/en/graphql/reference/mutations#updateissuecomment
     const query = `mutation($comment_node_id:String!, $comment_body:String!) {
         updateIssueComment(input: {id:$comment_node_id, body:$comment_body}) {
             issueComment{
-                node {
-                    id
-                }
+                lastEditedAt
             }
         }
     }`;
@@ -23,5 +21,5 @@ module.exports = async (github, comment_node_id, comment_body) => {
     const result = await github.graphql(query, variables);
 
     // result looks something like this: {addComment:{commentEdge:{node:{id:MDEyOklzc3VlQ29tbWVudDc2MzIxNjkyOQ==}}}}
-    return result.updateIssueComment.issueComment.node.id;
+    return result.updateIssueComment.issueComment.lastEditedAt;
 }
