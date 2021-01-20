f0cb8705ed3360454e9e3bca4e867352d4a3e2e1
Konrad Kleine <kkleine@redhat.com>
2021-01-20 22:39:32 +0100

    fixup

--- .github/workflows/js/update_issue_comment.js
+++ .github/workflows/js/update_issue_comment.js
@@ -1,13 +1,12 @@
 // update_issue_comment:
 //
-// Takes an issue's/PR's node ID and a comment body and creates a
-// new comment on the issue or PR. It returns the ID node of the
-// newly created comment. This let's you do GraphQL mutations later on.
-module.exports = async (github, issue_node_id, comment_body) => {
+// Updates an issue/PR comment with a new comment body. It returns the
+// ID node of updated comment.
+module.exports = async (github, comment_node_id, comment_body) => {
     
-    const query = `mutation($issue_node_id:String!, $comment_body:String!) {
-        addComment(input: {subjectId:$issue_node_id, body:$comment_body}) {
-            commentEdge{
+    const query = `mutation($comment_node_id:String!, $comment_body:String!) {
+        updateIssueComment(input: {subjectId:$comment_node_id, body:$comment_body}) {
+            issueComment{
                 node {
                     id
                 }
@@ -16,12 +15,12 @@ module.exports = async (github, issue_node_id, comment_body) => {
     }`;
 
     const variables = {
-        issue_node_id: issue_node_id,
+        comment_node_id: comment_node_id,
         comment_body: comment_body,
     }
     
     const result = await github.graphql(query, variables);
 
     // result looks something like this: {addComment:{commentEdge:{node:{id:MDEyOklzc3VlQ29tbWVudDc2MzIxNjkyOQ==}}}}
-    return result.addComment.commentEdge.node.id;
+    return result.updateIssueComment.issueComment.node.id;
 }
