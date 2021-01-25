202c515bbf728f760e6e69d9e7bc4bbc29ebe10b
Konrad Kleine <konrad.kleine@posteo.de>
2021-01-25 23:19:34 +0100

    Update main.cpp

--- hello-world/src/main.cpp
+++ hello-world/src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main(int argc, char * argv[]) {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World and Galina!" << std::endl;
     return 0;
 } 
