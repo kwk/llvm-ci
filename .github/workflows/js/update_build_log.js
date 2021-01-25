23efe9a98b34e55ed85fa89231b6bf00b8cea1dd
Konrad Kleine <kkleine@redhat.com>
2021-01-25 16:00:35 +0100

    Mess with buildlog

--- .github/workflows/js/update_build_log.js
+++ .github/workflows/js/update_build_log.js
@@ -1,48 +1,73 @@
-// update_build_log:
-//
-// Updates an issue/PR comment with a new comment body. It returns the
-// last edited field.
-module.exports = async (github, repo_owner, repo_name, issue_number, comment_id, summary, body) => {
-    
-    // Returns the first comment with an HTML comment that contains the given id.
-    async function findComment(issue_number, id) {
-        const { data: comments } = await github.issues.listComments({
-            owner: repo_owner,
-            repo: repo_name,
-            issue_number: issue_number,
+// update_build_log
+// (comment_id is optional, e.g. on first call)
+module.exports = async (github, context, core, summary, body, build_log_comment_id) => {
+
+    issue_number = github.event.issue.number
+
+    // Returns the comment with the given ID or None, if it exists or None if it doesn't
+    // exist or None was provided as the ID to search for. 
+    // See https://docs.github.com/en/rest/reference/issues#get-an-issue-comment
+    async function getCommentByID(build_log_comment_id) {
+        if (!build_log_comment_id) {
+            return None
+        }
+        // See https://octokit.github.io/rest.js/v18#issues-get-comment
+        return await github.issues.getComment({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            comment_id: build_log_comment_id,
         });
-        const regex = new RegExp(`<!--\\s*#${id}\\s*-->`);
-        console.log(comments);
-        return comments.filter(comment => comment.body.match(regex))[0];
     }
 
-    async function createOrUpdateComment(issue_number, id, message) {
-        const existingComment = await findComment(issue_number, id);
-        const body = `<!-- #${id} -->\n\n${message}`;
-        if (existingComment) {
-            console.log("Found comment. Old Body: " + existingComment.body);
-            await github.issues.updateComment({
-                ...context.repo,
-                comment_id: existingComment.id,
-                body,
-            });
-            return;
+    async function getTriggerComment() {
+        // The trigger comment ID is the ID of the comment that caused the workflow to fire.
+        trigger_comment_id = github.event.comment.id
+
+        const triggerComment = await getCommentByID(trigger_comment_id);
+        if (!triggerComment) {
+            core.setFailed(`Failed to get trigger comment with ID ${trigger_comment_id}.`);
         }
-        console.log("Creating new comment");
-        await github.issues.createComment({
+        return triggerComment;
+    }
+
+    async function createOrUpdateComment(issue_number, build_log_comment_id, message) {
+        const triggerComment = getTriggerComment();
+        const buildLogComment = await getCommentByID(build_log_comment_id);
+        const body = `${message}`;
+
+        if (buildLogComment) {
+            console.log("==== Found old buildLogComment. Old Body: " + buildLogComment.body);
+            return await github.issues.updateComment({
+                ...context.repo,
+                comment_id: buildLogComment.id,
+                body: buildLogComment.body + body,
+            });
+        }
+
+        console.log("==== Creating new buildLogComment");
+
+        // Upon creation, of build log comment, inform about the comment where this build log originated from.
+        body = `Build log for <a href="here">this comment</a>: ${triggerComment.body}\n${message}`
+
+        return await github.issues.createComment({
             ...context.repo,
             issue_number: issue_number,
             body,
         });
     }
-
+    
+    // Build log message template
     // TODO(kwk): Pepend summary with time (which timezone? -> UTC)?
-    msg = `<details>
-    <summary>`+summary+`</summary>
-    <p>
-        `+body+`
-    </p>
-    </details>`;
+    msg = `
+        <details>
+            <summary>
+                `+summary+`
+            </summary>
+            <p>
+                `+body+`
+            </p>
+        </details>
+    `;
 
-    return await createOrUpdateComment(issue_number, comment_id, msg);
+    return await createOrUpdateComment(issue_number, build_log_comment_id, msg);
 }
