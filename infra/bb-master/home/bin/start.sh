e1794111ad7a4561203fd0a326a33300aa931f73
Konrad Kleine <kkleine@redhat.com>
2021-12-16 21:54:52 +0100

    Test what happens if builder has more than one worker attached

--- infra/bb-master/home/bin/start.sh
+++ infra/bb-master/home/bin/start.sh
@@ -2,7 +2,6 @@
 
 set -eu
 
-set -x
 # Ensure Bash pipelines (e.g. cmd | othercmd) return a non-zero status if any of
 # the commands fail, rather than returning the exit status of the last command
 # in the pipeline.
